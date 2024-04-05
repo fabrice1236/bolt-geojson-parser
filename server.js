76739f09c088eb20a274158eccfa3c6663078705
Glitch (gbfs-to-geojson) <none>
2024-04-05 15:51:11 +0000

    🥈🌗 Updated with Glitch

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 
 const axios = require('axios');
-let BoltData;
-const BoltURL = 'https://mds.bolt.eu/gbfs/2/336/free_bike_status'; //MODIFY ME TO WHATEVER BOLT CITY ENDPOINT YOU'D LIKE
+const BoltURL = /*'https://mds.bolt.eu/gbfs/2/336/free_bike_status'*/'https://gbfs.api.ridedott.com/public/v2/brussels/free_bike_status.json'; //MODIFY ME TO WHATEVER BOLT CITY ENDPOINT YOU'D LIKE
 
 
 const fastify = require("fastify")({
@@ -9,30 +8,17 @@ const fastify = require("fastify")({
 });
 
 // ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
-
-async function getBoltData() {
-  try {
-    const response = await axios.get(BoltURL);
-    BoltData = response.data.data.bikes;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-let onIt = false
-
-fastify.get("/", async function (request, reply) {
+function convert(array) {
   let FeatureDic = [];
-  await getBoltData();
-  for (let i = 0; i < BoltData.length; i++) {
-    if (BoltData[i].is_reserved == false && BoltData[i].is_disabled == false) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].is_reserved == false && array[i].is_disabled == false) {
       let data = `{
       "type": "Feature",
       "properties": {},
       "geometry": {
         "coordinates": [
-          ${BoltData[i].lon},
-          ${BoltData[i].lat}
+          ${array[i].lon},
+          ${array[i].lat}
         ],
         "type": "Point"
       }
@@ -42,13 +28,50 @@ fastify.get("/", async function (request, reply) {
     }
 
   }
-  
   let final = `{
   "type": "FeatureCollection",
   "features": [${FeatureDic}]}`;
-  console.log(FeatureDic.length);
+  return final
+}
+
+
+async function getGBFSData(provider) {
+  let GBFSData;
+  try {
+    const response = await axios.get(provider);
+    GBFSData = response.data.data.bikes;
+    return GBFSData;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
+fastify.get("/bolt", async function (request, reply) {
+  let BoltData = await getGBFSData("https://mds.bolt.eu/gbfs/2/336/free_bike_status");
+  let final = convert(BoltData);
+  
+
   return reply.send(JSON.parse(final));
 });
+
+fastify.get("/dott", async function (request, reply) {
+  let DottData = await getGBFSData('https://gbfs.api.ridedott.com/public/v2/brussels/free_bike_status.json');
+  let final = convert(DottData);
+  
+
+  return reply.send(JSON.parse(final));
+});
+
+
+fastify.get("/lime", async function (request, reply) {
+  let LimeData = await getGBFSData('https://data.lime.bike/api/partners/v2/gbfs/brussels/free_bike_status');
+  let final = convert(LimeData);
+  
+
+  return reply.send(JSON.parse(final));
+});
+
 
 fastify.listen(
   { port: process.env.PORT, host: "0.0.0.0" },
